8d14f26cfa3d49d4c522ea1ab710c7c0a9bc8b73
azzaomer <48832222+azzaomer@users.noreply.github.com>
2025-11-15 10:44:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 
 // CORS Configuration - Add this RIGHT AFTER const app = express();
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3001', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
